3cd97f2207a9ee353f4a1fd808c81c7c4c258e1f
robkea <robe3046@stud.kea.dk>
2017-03-21 11:09:20 +0100

    my first java script

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = "Robert";
+
+let numKids = 0;
+
+let partner = "not found"
+
+let address = "Morbaenhaven";
+
+let jobTitle = "student";
+
+
+console.log("You will be " + name + " in " + address + " and married with " + partner + " with " + numKids + " kids " );
